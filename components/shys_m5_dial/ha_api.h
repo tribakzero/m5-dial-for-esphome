f3fa0b5b4179f53fa8385f84889689b83e98c84e
Ale Camarillo <g.camarillo@traxion.global>
2024-03-16 18:57:55 -0600

    Feat: Consider all lights

--- components/shys_m5_dial/ha_api.h
+++ components/shys_m5_dial/ha_api.h
@@ -9,6 +9,14 @@ namespace esphome
                 std::string colorValue;
                 esphome::api::HomeassistantServiceResponse resp;
                 esphome::api::HomeassistantServiceMap resp_kv;
+
+                void considerAllLights(String entity){
+                    if(entity == "light.all"){
+                        return "all";
+                    } else {
+                        return entity;
+                    }
+                }
                 
             public:
                 void turnLightOn(String entity, int brightness){
@@ -16,12 +24,12 @@ namespace esphome
                 }
 
                 void turnLightOn(String entity, int brightness, int color){
-                    ESP_LOGD("HA_API", "light on values: %s (%i) Color: %i", entity.c_str(), brightness, color);
+                    ESP_LOGD("HA_API", "light on values: %s (%i) Color: %i", considerAllLights(entity.c_str()), brightness, color);
 
                     resp.service = "light.turn_on";
                     
                     resp_kv.key = "entity_id";
-                    resp_kv.value = entity.c_str() == "light.all" ? "all" : entity.c_str();
+                    resp_kv.value = considerAllLights(entity.c_str());
                     resp.data.push_back(resp_kv);
                     
                     resp_kv.key = "brightness_pct";
@@ -30,15 +38,15 @@ namespace esphome
                     
                     if(color >= 0){
                         colorValue = str_sprintf("{{(%d,100)|list}}", color);
-                        ESP_LOGD("HA_API", "light on: %s (%i) HS-Color: %s", entity.c_str(), brightness, colorValue.c_str());
+                        ESP_LOGD("HA_API", "light on: %s (%i) HS-Color: %s", considerAllLights(entity.c_str()), brightness, colorValue.c_str());
 
                         resp_kv.key = "hs_color";
                         resp_kv.value = colorValue.c_str();
                         resp.data_template.push_back(resp_kv);                    
                         
-                        ESP_LOGI("HA_API", "turn light on: %s (%i) HS-Color: %s", entity.c_str(), brightness, colorValue.c_str());
+                        ESP_LOGI("HA_API", "turn light on: %s (%i) HS-Color: %s", considerAllLights(entity.c_str()), brightness, colorValue.c_str());
                     } else {
-                        ESP_LOGI("HA_API", "turn light on: %s (%i)", entity.c_str(), brightness);
+                        ESP_LOGI("HA_API", "turn light on: %s (%i)", considerAllLights(entity.c_str()), brightness);
                     }
 
                     esphome::api::global_api_server->send_homeassistant_service_call(resp);
@@ -51,7 +59,7 @@ namespace esphome
                     resp.service = "light.turn_off";
 
                     resp_kv.key = "entity_id";
-                    resp_kv.value = entity.c_str() == "light.all" ? "all" : entity.c_str();
+                    resp_kv.value = considerAllLights(entity.c_str());
                     resp.data.push_back(resp_kv);
 
                     esphome::api::global_api_server->send_homeassistant_service_call(resp);
@@ -66,12 +74,12 @@ namespace esphome
                     resp.service = "light.toggle";
 
                     resp_kv.key = "entity_id";
-                    resp_kv.value = "all";
+                    resp_kv.value = considerAllLights(entity.c_str());
                     resp.data.push_back(resp_kv);
 
                     esphome::api::global_api_server->send_homeassistant_service_call(resp);
                     
-                    ESP_LOGI("HA_API", "toggle light: %s", "all");
+                    ESP_LOGI("HA_API", "toggle light: %s", considerAllLights(entity.c_str()));
                 }
         };
 
